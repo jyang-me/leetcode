803ca7ad8ddbb5cbe8186ff2da4e7aa2ce0f9357
ljyang009-prog <ljyang009@gmail.com>
2026-07-09 02:21:57 +0800

    贪心算法

--- 122.c
+++ 122.c
@@ -1,3 +1,4 @@
+// 动态规划算法
 int maxProfit(int* prices, int pricesSize) {
     if (pricesSize < 2) return 0;
 
@@ -16,4 +17,15 @@ int maxProfit(int* prices, int pricesSize) {
     }
 
     return cash;
+}
+
+// 贪心算法
+int maxProfit(int* prices, int pricesSize) {
+    int profit = 0;
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1];
+        }
+    }
+    return profit;
 }
